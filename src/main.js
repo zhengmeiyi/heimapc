cd51b712f0bce1d2d716ea84471e430d839597aa
meiyi zheng <524875323@qq.com>
2020-03-01 19:44:35 +0800

    静态资源导入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'// 引入element-ui
 import 'element-ui/lib/theme-chalk/index.css'// 引入element-ui 的css文件
+import '@/styles/index.less'// 引入less文件
 Vue.config.productionTip = false
 Vue.use(ElementUI)// element-ui 全局注册
 new Vue({
