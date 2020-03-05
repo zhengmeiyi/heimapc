5ea2f95bedf23ed7f24c7c917f4a90d926fa22f7
meiyi zheng <524875323@qq.com>
2020-03-05 19:12:53 +0800

    axios拦截器-统一处理x响应及异常数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,8 @@
 import axios from 'axios' // 引入axios
+import router from '../router'
 // import router from '@/router' // 路由实例对象
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 配置基地址
-
+// axios拦截器 统一处理请求token
 axios.interceptors.request.use(function (config) {
   const token = localStorage.getItem('user_token')
   config.headers.Authorization = `Bearer ${token}`
@@ -9,5 +10,15 @@ axios.interceptors.request.use(function (config) {
 }, function (error) {
   return Promise.reject(error)
 })
-
+// axios拦截器 统一处理响应数据
+axios.interceptors.response.user(function (response) {
+  return response.data ? response.data : {}
+}, function (error) {
+  // 失败时执行
+  if (error.response.status === 401) {
+    localStorage.removeItem('user_token')
+    router.push('/login')
+  }
+  return Promise.reject(error)
+})
 export default axios
