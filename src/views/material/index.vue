e13bab9a8a23922f3475ab6e3675228be5bf6ebd
meiyi zheng <524875323@qq.com>
2020-03-06 21:07:52 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -5,12 +5,23 @@
           <template slot="title">素材管理</template>
           </bread-crumb>
       </el-card>
+      <el-row type="flex" style="margin:30px">
+        <el-tabs type="card" v-model="activeName">
+          <el-tab-pane label="全部素材" name="all">111111</el-tab-pane>
+          <el-tab-pane label="收藏素材" name="collect">2222222</el-tab-pane>
+        </el-tabs>
+      </el-row>
+
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all'
+    }
+  }
 }
 </script>
 
