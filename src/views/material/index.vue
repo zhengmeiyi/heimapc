070da4bebb0bd1f223e34d68068dfcda6b896fc2
meiyi zheng <524875323@qq.com>
2020-03-06 21:48:35 +0800

    素材列表-数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -6,9 +6,32 @@
           </bread-crumb>
       </el-card>
       <el-row type="flex" style="margin:30px">
-        <el-tabs type="card" v-model="activeName">
-          <el-tab-pane label="全部素材" name="all">111111</el-tab-pane>
-          <el-tab-pane label="收藏素材" name="collect">2222222</el-tab-pane>
+        <el-tabs type="card" v-model="activeName" @tab-click="changtab">
+          <el-tab-pane label="全部素材" name="all">
+              <div class="imglist">
+                <el-card v-for="item in list" :key="item.id" class="imgcard">
+                  <img :src="item.url" alt="" class="images">
+                  <el-row class="operate" type="flex" align="middle" justify="space-around">
+                        <i class='el-icon-star-on'></i>
+                        <i class='el-icon-delete-solid'></i>
+                  </el-row>
+
+              </el-card>
+              </div>
+
+          </el-tab-pane>
+          <el-tab-pane label="收藏素材" name="collect">
+              <div class="imglist">
+                <el-card v-for="item in list" :key="item.id" class="imgcard">
+                  <img :src="item.url" alt="" class="images">
+                  <el-row class="operate" type="flex" align="middle" justify="space-around">
+                        <i class='el-icon-star-on'></i>
+                        <i class='el-icon-delete-solid'></i>
+                  </el-row>
+
+              </el-card>
+              </div>
+          </el-tab-pane>
         </el-tabs>
       </el-row>
 
@@ -19,12 +42,57 @@
 export default {
   data () {
     return {
-      activeName: 'all'
+      activeName: 'all',
+      list: []
     }
+  },
+  methods: {
+    changtab () {
+      this.getimages()
+    },
+    getimages () {
+      this.$axios({
+        url: '/user/images',
+        collect: this.activeName === 'collect'
+
+      }).then((res) => {
+        console.log(res)
+        this.list = res.data.results
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  },
+  created () {
+    this.getimages()
   }
 }
+
 </script>
 
-<style>
+<style lang="less" scoped>
+.imglist {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    .imgcard {
+        margin: 7px;
+        width: 220px;
+        height: 240px;
+    .images {
+        width:100%;
+        height:100%;
+    }
+    .operate {
+        height: 30px;
+        background: #f4f5f6;
+        i {
+            font-size:20px;
+        }
+    }
+
+}
+}
 
 </style>
