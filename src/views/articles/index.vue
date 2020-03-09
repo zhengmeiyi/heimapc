27e4c5126a8413e7d03627ba6ba66a8703911ca0
meiyi zheng <524875323@qq.com>
2020-03-09 14:29:32 +0800

    内容列表-页面主体结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -24,8 +24,27 @@
                   <el-date-picker type='daterange' v-model="searchForm.dateRange">
                     </el-date-picker>
               </el-form-item>
-
           </el-form>
+          <div class="p">
+            <p>共找到1000条符合条件的内容</p>
+          </div>
+          <div class="newslist">
+            <div class="left">
+              <div class="cover">
+              <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583743409233&di=b511298fd46d784374d5efae1d81b7f8&imgtype=0&src=http%3A%2F%2Fi-1.shouji56.com%2F2014%2F8%2F28%2Fdd41b9e1-fd62-4f93-b20f-48d476777b45.jpg" alt="">
+              </div>
+              <div class="info">
+               <p>11111111111</p>
+               <el-tag type="success" style="width:60px">已发表</el-tag>
+               <p class="data">2020-03-09 13:40:22</p>
+              </div>
+            </div>
+
+            <div class="right">
+              <i class="el-icon-edit"> 修改</i>
+              <i class="el-icon-delete"> 删除</i>
+            </div>
+          </div>
       </el-card>
   </div>
 </template>
@@ -57,6 +76,48 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.p{
+  height: 35px;
+  border-bottom: 1px dashed #ccc;
+}
+.newslist {
+  height: 150px;
+  border-bottom: 1px solid #ccc;
+  .left {
+    .cover {
+      height: 120px;
+      width: 180px;
+      margin: 15px 10px;
+      float: left;
+      img {
+        height: 100%;
+        width: 100%;
+        border-radius: 4px;
+      }
+    }
+    .info {
+      height: 120px;
+      margin: 15px 0px;
+      float: left;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      .data{
+        font-size: 12px;
+        color: #999;
+      }
+    }
+  }
+  .right {
+    float: right;
+    i {
+      font-size: 12px;
+      margin-top:30px;
+      margin-right:10px;
+      cursor: pointer;
+      user-select: none;
+    }
+  }
+}
 </style>
