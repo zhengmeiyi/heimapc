1a8c968ca3a58e0fc77374c92255cef1e61c8d89
meiyi zheng <524875323@qq.com>
2020-03-09 19:05:00 +0800

    发表文章-页面结构-简单实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>111</div>
+ <el-card>
+   <bread-crumb slot="header">
+    <template slot="title">发布文章</template>
+   </bread-crumb>
+   <el-form style="margin-left:50px">
+     <el-form-item label="标题：">
+       <el-input style="width:500px" placeholder="请输入您的标题"></el-input>
+     </el-form-item>
+     <el-form-item label="内容：">
+       <el-input type="textarea" rows="12" style="width:60%" placeholder="请输入您的内容" ></el-input>
+     </el-form-item>
+      <el-form-item label="封面：">
+       <el-radio-group>
+         <el-radio label="">单图</el-radio>
+         <el-radio label="">三图</el-radio>
+         <el-radio label="">无图</el-radio>
+         <el-radio label="">自动</el-radio>
+       </el-radio-group>
+     </el-form-item>
+     <el-form-item label="频道：">
+       <el-select placeholder="请选择频道">
+         <el-option>qq</el-option>
+       </el-select>
+     </el-form-item>
+     <el-form-item style="margin-left:60px">
+       <el-button type="success" round>发布文章</el-button>
+       <el-button type="info" round>存入草稿</el-button>
+     </el-form-item>
+   </el-form>
+ </el-card>
 </template>
 
 <script>
