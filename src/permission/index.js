199aca151906dbec1a39decad5fe2163284d8ca6
meiyi zheng <524875323@qq.com>
2020-03-11 22:03:34 +0800

    黑马头条PC-页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,6 +1,10 @@
 import router from '@/router'
+import progress from 'nprogress' // 进度条包
+import 'nprogress/nprogress.css' // 样式文件
 // 设置token权限
 router.beforeEach(function (to, from, next) {
+  // 开启进度条
+  progress.start()
   if (to.path.startsWith('/home')) { // 如果去主页
     const token = window.localStorage.getItem('user_token')
     if (token) { // 去主页判断有没有token,有就通过，没有就去login
@@ -11,4 +15,8 @@ router.beforeEach(function (to, from, next) {
   } else { // 如果不去主页通过
     next()
   }
+})
+
+router.afterEach(() => {
+  progress.done() // 关闭进度条
 })
